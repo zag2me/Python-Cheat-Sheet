42492de0717582d128e76ea530076b94dded8be1
Zag <zag2me@gmail.com>
2019-05-17 10:47:34 +0100

    [Add] First cheatsheet

---
+++ main.py
@@ -0,0 +1,19 @@
+# Header
+print ("######################")
+print ("# Python Cheat Sheet #")
+print ("######################")
+
+# Example 1 Hello World!
+print("Hello World!")
+
+# Example 2 Variable 
+x = 1
+print ("The number is:", x)
+
+# Example 3 List
+myList = ["Apple", "Orange", "Banana"]
+print (myList[1])
+
+# Example 4 Loop through a list
+for x in myList:
+  print(x)
